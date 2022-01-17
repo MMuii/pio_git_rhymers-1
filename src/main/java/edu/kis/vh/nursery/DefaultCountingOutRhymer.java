36f4b92b25d05eddc91aa04f1cb53957cf8bdea5
Marcin Świderek <muimuikrol@gmail.com>
2022-01-17 22:58:17 +0100

    1.4.3.2 - generate getter for total attribute

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -30,6 +30,10 @@ public class DefaultCountingOutRhymer {
         return numbers[total];
     }
 
+    public int getTotal() {
+        return total;
+    }
+
     public int countOut() {
         if (callCheck())
             return EMPTY_LIST_RETURN_VALUE;
